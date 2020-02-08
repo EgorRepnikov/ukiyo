2d817a6158b92f3e5b460a3b5312862d827c509a
EgorRepnikov <egorepnikov@gmail.com>
2020-02-08 22:47:52 +0300

    Create deep getFilesList

--- server/lib/config.js
+++ server/lib/config.js
@@ -1,3 +1,4 @@
 module.exports = {
   GOOGLE_API_KEY: process.env.GOOGLE_API_KEY,
+  MUSIC_TYPES: ['mp3']
 }
--- server/storages/google-drive.js
+++ server/storages/google-drive.js
@@ -1,13 +1,31 @@
 const { config, get, readJson } = require('../lib')
 
-exports.getFilesList = async (folderId) => {
+exports.getFilesList = async folderId => {
   const res = await get('https://www.googleapis.com/drive/v3/files', {
     key: config.GOOGLE_API_KEY,
     q: `'${folderId}' in parents`
   })
   const body = await readJson(res)
-  return body.files
+  const result = []
+  for (const item of body.files) {
+    if (item.mimeType === 'application/vnd.google-apps.folder') {
+      result.push({
+        kind: 'folder',
+        id: item.id,
+        files: await this.getFilesList(item.id),
+      })
+    } else if (config.MUSIC_TYPES.includes(getFileExtension(item.name))) {
+      result.push({
+        kind: 'file',
+        id: item.id,
+        name: item.name,
+      })
+    }
+  }
+  return result
 }
+
+const getFileExtension = (name) => name.split('.').pop()
 
 exports.pipeFile = async (fileId, stream) => {
   const res = await get(`https://www.googleapis.com/drive/v3/files/${fileId}`, {
