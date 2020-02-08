416838aee0be876ffb1d0c746f49752ecf2c9d5e
EgorRepnikov <egorepnikov@gmail.com>
2020-02-08 22:06:21 +0300

    Create Google Drive client

---
+++ server/storages/google-drive.js
@@ -0,0 +1,18 @@
+const { get, readJson } = require('../lib/utils')
+
+exports.getFilesList = async (folderId) => {
+  const res = await get('https://www.googleapis.com/drive/v3/files', {
+    key: process.env.GOOGLE_KEY,
+    q: `'${folderId}' in parents`
+  })
+  const body = await readJson(res)
+  return body.files
+}
+
+exports.pipeFile = async (fileId, stream) => {
+  const res = await get(`https://www.googleapis.com/drive/v3/files/${fileId}`, {
+    key: process.env.GOOGLE_KEY,
+    alt: 'media'
+  })
+  res.pipe(stream)
+}
