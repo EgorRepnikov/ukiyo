fa8e97b5b74f0ec7fecf35f0c46c66eede9981ab
EgorRepnikov <egorepnikov@gmail.com>
2020-02-15 15:54:20 +0300

    Token auto setting on start up

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import { useRoutes } from 'hookrouter'
+import jwtDecode from 'jwt-decode'
 
 import store from './store'
 import { Login, Register } from './components'
+import { setAuthToken } from './utils'
+import { setCurrentUser, logout } from './actions/auth'
+
+if (localStorage.access_token) {
+  const { access_token } = localStorage
+  setAuthToken(access_token)
+  const decoded = jwtDecode(access_token)
+  store.dispatch(setCurrentUser(decoded))
+  const currentTime = Date.now() / 1000
+  if (decoded.exp < currentTime) {
+    store.dispatch(logout())
+    window.location.href = '/login'
+  }
+}
 
 const routes = {
   '/login': () => <Login />,
